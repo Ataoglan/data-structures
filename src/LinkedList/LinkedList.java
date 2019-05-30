029db9df283755dbf87d96676bd3dbc6c499ae2e
Ataoglan <ozcanataoglan@gmail.com>
2019-05-30 12:50:07 +0100

    first commit

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+
+public class LinkedList {
+
+    Node head; //head of list
+
+     static class Node {
+        int data;
+        Node next;
+
+        Node(int d){data = d; next=null;}
+    }
+
+
+}
+
